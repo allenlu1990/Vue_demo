1a3bed80abfa3d03ef6fbfe48fd64db572512bca
alu <alu@Allens-MacBook.local>
2020-04-28 22:17:17 +0800

    add

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
+/* eslint-disable */
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://localhost:8080/test/'
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false;
-
+Vue.use(ElementUI);
 new Vue({
   router,
   store,
--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,32 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      userlist: []
+    }
+  },
+  created () {
+    this.getUserList()
+  },
+  methods: {
+    async getUserList () {
+      const { data: res } = await this.$http.get('test', {
+        params: this.queryInfo
+      })
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取用户列表失败！')
+      }
+      this.userlist = res.data.users
+      this.totle = res.data.totle
+    }
+    
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
